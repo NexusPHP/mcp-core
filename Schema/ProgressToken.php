c5250fd3378fe4f176014780bb168a260d2d8a40
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-03-09 18:26:06 +0800

    Add `ProgressToken`

---
+++ Schema/ProgressToken.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Nexus MCP SDK package.
+ *
+ * (c) 2026 John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Nexus\Mcp\Core\Schema;
+
+use Nexus\Assert\Assert;
+
+/**
+ * A progress token, used to associate progress notifications with the original request.
+ */
+final readonly class ProgressToken
+{
+    /**
+     * @var int|non-empty-string
+     */
+    public int|string $token;
+
+    public function __construct(int|string $token)
+    {
+        if (\is_string($token)) {
+            Assert::that($token)->isNonEmptyString('Progress token must be a non-empty string.');
+        }
+
+        $this->token = $token;
+    }
+}

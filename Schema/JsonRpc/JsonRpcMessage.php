ed9f890dfb2fd408cc1b19759ca057c00a241f7c
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-04-25 21:43:11 +0800

    Add `JsonRpcMessage` interface

---
+++ Schema/JsonRpc/JsonRpcMessage.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Nexus MCP SDK package.
+ *
+ * (c) 2026 John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Nexus\Mcp\Core\Schema\JsonRpc;
+
+/**
+ * Refers to any valid JSON-RPC object that can be decoded off the wire, or encoded to be sent.
+ */
+interface JsonRpcMessage extends \JsonSerializable
+{
+    public const string JSONRPC_VERSION = '2.0';
+}

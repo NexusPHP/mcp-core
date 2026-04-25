8d02b9bd180bcf3368a34304d417627860517a4b
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-04-25 22:07:58 +0800

    Add notification markers

---
+++ Schema/Notification/ServerNotification.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Nexus MCP SDK package.
+ *
+ * (c) 2026 John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Nexus\Mcp\Core\Schema\Notification;
+
+/**
+ * Marker for a notification that a server may send to a client.
+ */
+interface ServerNotification
+{
+}

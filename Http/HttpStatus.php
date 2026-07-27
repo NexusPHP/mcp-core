7009ae0f89c3ba909e43f6c29c11f09c86ad2071
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-07-27 15:03:05 +0800

    Present bearer tokens and recover from authorization challenges

--- Http/HttpStatus.php
+++ Http/HttpStatus.php
@@ -24,6 +24,7 @@ enum HttpStatus: int
     case Accepted = 202;
     case NoContent = 204;
     case BadRequest = 400;
+    case Unauthorized = 401;
     case Forbidden = 403;
     case NotFound = 404;
     case MethodNotAllowed = 405;
